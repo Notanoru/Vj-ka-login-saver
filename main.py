2bcf877fd31b78a7a5333bbf4f6895bebef1c84c
Notanoru <167917687+Notanoru@users.noreply.github.com>
2024-06-30 10:36:26 +0530

    Update main.py

--- main.py
+++ main.py
@@ -63,8 +63,8 @@ def progress(current, total, message, type):
 # start command
 @bot.on_message(filters.command(["start"]))
 def send_start(client: pyrogram.client.Client, message: pyrogram.types.messages_and_media.message.Message):
-	bot.send_message(message.chat.id, f"**__👋 Hi** **{message.from_user.mention}**, **I am Save Restricted Bot, I can send you restricted content by it's post link__**\n\n{USAGE}",
-	reply_markup=InlineKeyboardMarkup([[ InlineKeyboardButton("🌐 Update Channel", url="https://t.me/VJ_Botz")]]), reply_to_message_id=message.id)
+	bot.send_message(message.chat.id, f"**__👋 Hi** **{message.from_user.mention}**, **I am Save Restricted Bot made by SHIMPERD 💝, I can send you restricted content by it's post link__**\n\n{USAGE}",
+	reply_markup=InlineKeyboardMarkup([[ InlineKeyboardButton("🌐 Update Channel", url="https://t.me/+mVX1hOZ9CgFkNDk9")]]), reply_to_message_id=message.id)
 
 
 @bot.on_message(filters.text)
